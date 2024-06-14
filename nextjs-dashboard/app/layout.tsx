fb312db4f35c1fab77101e76cf74d1bde6a0929e
YuHyeonWook <yho7955@naver.com>
2024-06-14 19:39:24 +0900

    init commit

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,5 +1,5 @@
-import '@/app/ui/global.css'
-import { inter} from './ui/fonts';
+import '@/app/ui/global.css';
+import { inter } from './ui/fonts';
 
 export default function RootLayout({
   children,
@@ -7,9 +7,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}
-      >{children}</body>
+    <html lang="ko">
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
--- nextjs-dashboard/app/ui/fonts.ts
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -1,9 +1,9 @@
-import {Inter, Lobster, Lusitana  } from 'next/font/google'
+// next/font 대한 정보를 가져오는 코드입니다.
+import { Inter, Lobster, Lusitana } from 'next/font/google';
 
-
-export const inter = Inter({subsets: ['latin']})
-export const lobster = Lobster({ weight: '400',  subsets: ['latin'] });
+export const inter = Inter({ subsets: ['latin'] });
+export const lobster = Lobster({ weight: '400', subsets: ['latin'] });
 export const lusitana = Lusitana({
-    weight: ['400', '700'],
-    subsets: ['latin'],
-  });
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
